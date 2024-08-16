918fe9ef3411ca791f141e5ce8e0d5f201d1c63a
Gideon <gideonphiri032@gmail.com>
2024-08-16 02:56:41 +0000

    0-lockboxes.py updated

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,16 +1,37 @@
-#!/usr/bin/python3
+
+ckboxes.py
+This module contains the function canUnlockAll that determines if all boxes 
+in a list of boxes can be opened starting from the first box.
+"""
 
 def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be unlocked.
+    
+    :param boxes: List of lists where each sublist represents keys inside a box.
+                  A key with the same number as a box opens that box.
+    :return: True if all boxes can be unlocked, otherwise False.
+    """
+    # A set to keep track of all opened boxes
     opened_boxes = set()
-    stack = [0]  # Start with the first box
+    
+    # Initialize the stack with the first box (box 0 is always open)
+    stack = [0]
 
     while stack:
-        current_box = stack.pop()  # Get the last box in the stack
-        if current_box not in opened_boxes:
-            opened_boxes.add(current_box)  # Mark this box as opened
-            for key in boxes[current_box]:
-                if key < len(boxes) and key not in opened_boxes:
-                    stack.append(key)  # Add all keys to the stack
+        # Pop the last box from the stack
+        current_box = stack.pop()
 
-    # Check if we've opened all the boxes
+        # If this box has not been opened yet
+        if current_box not in opened_boxes:
+            # Mark the current box as opened
+            opened_boxes.add(current_box)
+
+            # Add keys found in the current box to the stack
+            for key in boxes[current_box]:
+                # Ensure the key corresponds to a valid box that hasn't been opened
+                if key < len(boxes) and key not in opened_boxes:
+                    stack.append(key)
+
+    # If the number of opened boxes equals the total number of boxes, return True
     return len(opened_boxes) == len(boxes)

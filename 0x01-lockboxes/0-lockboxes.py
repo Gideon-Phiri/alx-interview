13bfb3f3db5d4f4b26a64f3bd640a5147bb515f9
Gideon <gideonphiri032@gmail.com>
2024-09-05 18:38:37 +0000

    second fix

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,33 +1,35 @@
 #!/usr/bin/python3
 """
 0-lockboxes.py
-This module contains the canUnlockAll function.
+This module contains the canUnlockAll function which determines
+if all boxes can be unlocked given a set of keys.
 """
 
 
 def canUnlockAll(boxes):
     """
     Determines if all boxes can be unlocked.
-
     Args:
-        boxes (list):
-           - A list of lists, where each list contains keys to other boxes.
-
+        boxes (list of lists):
+                            - A list of lists
+                            - where each list contains keys to other boxes.
     Returns:
         bool: True if all boxes can be unlocked, False otherwise.
     """
+    if not boxes:
+        return False  # No boxes exist
+
     n = len(boxes)  # Number of boxes
-    unlocked = {0}  # Set of unlocked boxes, starting with box 0
-    stack = boxes[0]  # Start with keys from the first box
+    unlocked = [False] * n
+    unlocked[0] = True  # The first box is unlocked by default
+    stack = boxes[0]  # Initialize stack with keys from the first box
 
-    # Process the stack to unlock boxes
+    # Process the stack
     while stack:
-        key = stack.pop()  # Take a key from the stack
-
-        # If the key unlocks a new box
-        if key < n and key not in unlocked:
-            unlocked.add(key)  # Mark the box as unlocked
-            stack.extend(boxes[key])  # Add keys from the newly unlocked box
-
-    # If all boxes are unlocked, return True; else return False
-    return len(unlocked) == n
+        key = stack.pop()
+        # Ensure the key unlocks a valid, locked box
+        if 0 <= key < n and not unlocked[key]:
+            unlocked[key] = True  # Unlock the box
+            stack.extend(boxes[key])
+    # Check if all boxes are unlocked
+    return all(unlocked)
